f837ec310be5495c02048874f4b0dc01d128c5ca
surajjayraman <suraj.jayraman@gmail.com>
2024-03-04 15:26:25 -0500

    Update Register page

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import "@styles/Register.scss";
+import { FcGoogle } from "react-icons/fc";
 
-const RegisterPage = () => {
+const Register = () => {
   return (
-    <div>Register Page</div>
-  )
-}
+    <div className="register">
+      <img src="/assets/register.jpg" alt="register" className="register_decor" />
+      <div className="register_content">
+        <form className="register_content_form">
+          <input type="text" placeholder="Username" name="username" required />
+          <input type="email" placeholder="Email" name="email" required />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            required
+          />
+          <input
+            id="image"
+            type="file"
+            name="profileImage"
+            accept="image/*"
+            style={{display: "none"}}
+            required
+          />
+          <label htmlFor="image">
+            <img src="/assets/addImage.png" alt="add profile"></img>
+            <p>Upload Profile Photo</p>
+          </label>
+          <button type="submit">Register</button>
+        </form>
+        <button className="google" type="button" onClick={()=> {}}>
+          <p>Log In with Google</p>
+          <FcGoogle />
+        </button>
+        <a href="/login">Already have an account? Log In Here</a>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default Register;
