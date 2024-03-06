4d2cd30b682819c6f98f32816e7ed49dddd9aa69
surajjayraman <suraj.jayraman@gmail.com>
2024-03-06 16:40:28 -0500

    Update login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,20 +1,36 @@
+"use client";
 import "@styles/login.scss";
 import { useState } from "react";
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    name === "email" ? setEmail(value) : setPassword(value);
+  };
 
   return (
     <div className="login">
       <img src="/assets/login.jpg" alt="login" className="login_decor"></img>
       <div className="login_content">
         <form>
-          <input type="email" placeholder="Email" name="email" required></input>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            required
+            onChange={handleChange}
+          ></input>
           <input
             type="password"
             placeholder="Password"
             name="password"
+            value={password}
             required
+            onChange={handleChange}
           ></input>
           <button type="submit">Log In</button>
         </form>
