704a7bf637feb4bdbc4814ef1b538a74929cbbc6
surajjayraman <suraj.jayraman@gmail.com>
2024-03-06 12:03:26 -0500

    Update auth handler with callbacks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import User from "@models/User";
+import { connectToDatabase } from "@mongodb/database";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -15,6 +17,37 @@ const handler = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        //connect to database
+        try {
+          await connectToDatabase(process.env.MONGO_URL);
+          let user = await User.findOne({ email: profile.email });
+          if (!user) {
+            user = await User.create({
+              email: profile.email,
+              username: profile.name,
+              profileImagePath: profile.image,
+              wishlist: [],
+              cart: [],
+              order: [],
+              work: [],
+            });
+          }
+          return user;
+        } catch (error) {
+          console.log(`Error checking if user exists: $error.message`);
+        }
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
