191e42f120bb4fec7093876aa97ba1e82a4900fb
surajjayraman <suraj.jayraman@gmail.com>
2024-03-06 18:02:20 -0500

    Update login page styles

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import "@styles/login.scss";
+import { signIn } from "next-auth/react";
 import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -10,12 +12,32 @@ const Login = () => {
     const { name, value } = e.target;
     name === "email" ? setEmail(value) : setPassword(value);
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const login_form = new FormData();
+    login_form.append("email", email);
+    login_form.append("password", password);
+    try {
+      const response = await fetch("/api/login/", {
+        method: "POST",
+        body: login_form,
+      });
+      if (response.ok) {
+        console.log("Login successful!");
+      }
+    } catch (err) {
+      console.log("Login failed!", err.message);
+    }
+  };
+  const loginWithGoogle = () => {
+    signIn("google", { callbackUrl: "/" });
+  };
 
   return (
     <div className="login">
       <img src="/assets/login.jpg" alt="login" className="login_decor"></img>
       <div className="login_content">
-        <form>
+        <form className="login_content_form">
           <input
             type="email"
             placeholder="Email"
@@ -34,6 +56,11 @@ const Login = () => {
           ></input>
           <button type="submit">Log In</button>
         </form>
+        <button type="button" onClick={loginWithGoogle} className="google">
+          <p>Log In with Google</p>
+          <FcGoogle />
+        </button>
+        <a href="/register">Don't have an account? Sign In Here</a>
       </div>
     </div>
   );
