b5e96fe1d24edfd6f8a0f79f676436384c88849a
surajjayraman <suraj.jayraman@gmail.com>
2024-03-06 16:30:54 -0500

    Update login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import "@styles/login.scss";
+import { useState } from "react";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-export default Login
+  return (
+    <div className="login">
+      <img src="/assets/login.jpg" alt="login" className="login_decor"></img>
+      <div className="login_content">
+        <form>
+          <input type="email" placeholder="Email" name="email" required></input>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          ></input>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
