78d88d0ab453cd5860afe4177c1aa4fae6aa3d44
surajjayraman <suraj.jayraman@gmail.com>
2024-03-07 15:43:57 -0500

    Add page EditWork

--- app/edit-work/page.jsx
+++ app/edit-work/page.jsx
@@ -3,15 +3,7 @@ import React, { useState } from "react";
 import Form from "@components/Form";
 import Navbar from "@components/Navbar";
 
-const CreateWork = () => {
-  const [work, setWork] = useState({
-    creator: "",
-    category: "",
-    title: "",
-    description: "",
-    price: "",
-    photos: [],
-  });
+const EditWork = () => {
   return (
     <>
       <Navbar />
@@ -20,4 +12,4 @@ const CreateWork = () => {
   );
 };
 
-export default CreateWork;
+export default EditWork;
