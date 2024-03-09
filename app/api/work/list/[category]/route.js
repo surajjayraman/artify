fb269ec1b5063f417698c634eb07bd2fa464a7d3
surajjayraman <suraj.jayraman@gmail.com>
2024-03-09 08:29:29 -0500

    Add api route for GET Work list by category

---
+++ app/api/work/list/[category]/route.js
@@ -0,0 +1,36 @@
+import { connectToDatabase } from "@mongodb/database";
+import Work from "@models/Work";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDatabase();
+    const category = params;
+    let workList;
+    if (category !== "All") {
+      workList = await Work.find({ category }).populate("creator");
+    } else {
+      workList = await Work.find().populate("creator");
+    }
+    if (!workList) {
+      return NextResponse.json({
+        status: 404,
+        body: {
+          message: "Work List not found!",
+        },
+      });
+    }
+    return NextResponse.json({
+      status: 200,
+      body: workList,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      status: 500,
+      body: {
+        message: "Work List fetch failed!",
+      },
+    });
+  }
+};
