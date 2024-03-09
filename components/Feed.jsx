487680743a46901612f64791a5ab8384fc4ec51e
surajjayraman <suraj.jayraman@gmail.com>
2024-03-09 09:44:03 -0500

    Update component Feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,22 @@
+"use client"
 import { categories } from "@data"; // Import the categories array from the data module
+import WorkList from "./WorkList";
+import { useState } from "react";
+import "@styles/Categories.scss";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
   return (
     <>
       <div className="categories">
         {categories?.map((item, index) => (
-          <p key={index}>{item}</p>
+          <p
+            key={index}
+            onClick={() => selectedCategory(item)}
+            className={`${item === selectedCategory ? "selected" : ""}`}
+          >
+            {item}
+          </p>
         ))}
       </div>
       <WorkList />
