4bdc51d4514363c4c7808f940bd4f29f1eb9367c
surajjayraman <suraj.jayraman@gmail.com>
2024-03-10 19:55:50 -0400

    Update page UpdateWork

--- app/update-work/page.jsx
+++ app/update-work/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const UpdateWork = () => {
-  return (
-    <div>UpdateWork</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const workId = searchParams.get("id");
+  const [work, setWork] = useState({
+    category: "",
+    title: "",
+    description: "",
+    price: "",
+    photos: [],
+  });
+  useEffect(() => {
+    const getWorkDetails = async () => {
+      if (workId) {
+        fetch(`/api/work/${workId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            setWork({
+              category: data.body.category,
+              title: data.body.title,
+              description: data.body.description,
+              price: data.body.price,
+              photos: data.body.workPhotoPaths,
+            });
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("Update Work getWorkDetails failed:", error);
+          });
+      }
+    };
+    getWorkDetails();
+  }, [workId]);
+  return <div>UpdateWork</div>;
+};
 
-export default UpdateWork
+export default UpdateWork;
