728c58a51a5d223d263c97926fe085a1801a572b
surajjayraman <suraj.jayraman@gmail.com>
2024-03-11 12:16:26 -0400

    Update User shop page

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+"use client";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Shop = () => {
+  const [loading, setLoading] = useState(true);
+  const { data: session } = useSession();
+  const loggedInUserId = session?.user?._id;
+
+  const searchParams = useSearchParams;
+  const [workList, setWorkList] = useState([]);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const getWorkList = () => {
+      if (loggedInUserId) {
+        fetch(`/api/user/${loggedInUserId}/shop`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(`Shop data:`, data);
+            setUser(data.user);
+            setWorkList(data.workList);
+            setLoading(false);
+          });
+      }
+    };
+    getWorkList();
+  }, [loggedInUserId]);
   return <div>Shop</div>;
 };
 
