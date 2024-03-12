656b7a8bfe1f2fd719f5d3163f8c80ded50e01f2
surajjayraman <suraj.jayraman@gmail.com>
2024-03-12 10:48:13 -0400

    Update SearchPage with getWorkList

--- app/search/[query]/page.jsx
+++ app/search/[query]/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useParams } from "next/navigation";
+import React, { use, useState } from "react";
 
 const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
-}
+  const { query } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [workList, setWorkList] = useState([]);
 
-export default SearchPage
+  useEffect(() => {
+    const getWorkList = async () => {
+      try {
+        const response = await fetch(`/api/work/search/${query}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+        setWorkList(data.body.works);
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getWorkList();
+  }, [query]);
+
+  return <div>SearchPage</div>;
+};
+
+export default SearchPage;
