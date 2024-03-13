ae204a4fee94f50f685a8463e690ff94c9205647
surajjayraman <suraj.jayraman@gmail.com>
2024-03-13 12:29:41 -0400

    Update page Cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import Navbar from "@components/Navbar";
+import "@styles/Cart.scss";
+import { useSession } from "next-auth/react";
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { data: session, update } = useSession();
+  const cart = session?.user?.cart;
+  const userId = session?.user?._id;
 
-export default Cart
+  const updateCart = async (cart) => {
+    const response = await fetch(`/api/user/${userId}/cart`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ cart }),
+    });
+    const data = await response.json();
+    console.log(`updated cart: ${JSON.stringify(data)}`);
+    update({ user: { cart: data } });
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="cart">Cart</div>;
+    </>
+  );
+};
+
+export default Cart;
