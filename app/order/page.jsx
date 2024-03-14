50a7fce6886ed33db30a83db786e853d1ae97b3a
surajjayraman <suraj.jayraman@gmail.com>
2024-03-14 13:15:32 -0400

    Update Order page

--- app/order/page.jsx
+++ app/order/page.jsx
@@ -1,8 +1,49 @@
-import '@styles/Order.scss'
+"use client";
+import Loader from "@components/Loader";
+import Navbar from "@components/Navbar";
+import "@styles/Order.scss";
+import { useSession } from "next-auth/react";
 const Order = () => {
-  return (
-    <div>Order</div>
-  )
-}
+  const { data: session } = useSession();
+  const orders = session?.user?.orders;
+  return !orders ? (
+    <Loader />
+  ) : (
+    <>
+      <Navbar />
+      <div className="orders">
+        <h1>Your Orders</h1>
+        <div className="order-list">
+          {orders?.map((order, index) => (
+            <div className="order" key={index}>
+              <div className="order-title">
+                <h4>Order ID: {order.id}</h4>
+                <h2>Total Paid: ${order.amountPaid}</h2>
+              </div>
+              <div className="order-items">
+                {order?.orderItems.map((item, index) => (
+                  <div className="product" key={index}>
+                    <div className="product-info">
+                      <img src={item.image} alt={item.title} />
+                      <div className="orderItemInfo">
+                        <h4>{item.title}</h4>
+                        <p>Product ID: {item.productId}</p>
+                      </div>
+                    </div>
 
-export default Order
+                    <div className="product-info2">
+                      <h3>Price: ${item.price}</h3>
+                      <p>Quantity: {item.quantity}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Order;
