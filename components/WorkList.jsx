d4bed39945a33318d9952c05a757c16e274a5713
surajjayraman <suraj.jayraman@gmail.com>
2024-03-15 13:12:43 -0400

    Update WorkList component

--- components/WorkList.jsx
+++ components/WorkList.jsx
@@ -1,8 +1,11 @@
 import "@styles/WorkList.scss";
 import WorkCard from "./WorkCard";
+import Loader from "./Loader";
 const WorkList = ({ data }) => {
   console.log(`data`, data);
-  return (
+  return !data ? (
+    <Loader />
+  ) : (
     <div className="work-list">
       {data?.map((work) => (
         <WorkCard key={work?._id} work={work} />
