c4a245797069aa2a5bd7232dd60427932b4ab5e1
surajjayraman <suraj.jayraman@gmail.com>
2024-03-16 18:44:13 -0400

    Update mongodb connectToDatabase

--- mongodb/database.js
+++ mongodb/database.js
@@ -5,10 +5,10 @@ let isConnected = false; // Track the connection
 
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
-  if (isConnected) {
-    console.log("MongoDB is already connected successfully!");
-    return;
-  }
+  // if (isConnected) {
+  //   console.log("MongoDB is already connected successfully!");
+  //   return;
+  // }
   // use new database connection
   try {
     const db = await mongoose.connect(process.env.MONGO_URL, {
